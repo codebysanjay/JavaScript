66186dda3834e9f862c8ea921548815ea86183bf
sanjaygecb <62655533+sanjaygecb@users.noreply.github.com>
2020-07-11 20:59:55 +0530

    Update Love-calculator.js

--- Love-calculator.js
+++ Love-calculator.js
@@ -3,7 +3,7 @@ prompt("What is lover's name ?")
 
 var lovescore=Math.random()*100;
 lovescore=Math.floor(lovescore)+1;
-if (lovescore>70){
+if (lovescore>=70){
      alert("Your score is "+lovescore+ "%. You Guys are great lovers ❤️ :)")
 }else if (lovescore<70||lovescore>20 ){
      alert("Your score is "+lovescore+"% .You Guys love as much as you can.")
