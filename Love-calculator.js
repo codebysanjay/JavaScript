7cefdb0a572675ae41505c2bce47fb9238d0a765
sanjaygecb <62655533+sanjaygecb@users.noreply.github.com>
2020-07-11 20:50:21 +0530

    A love calculator made using javascript
    In this program you can enter two names.The love calculator will give you the percentage score of the love between you. The program uses a random function to generate the score irrespective of the names we enter.

---
+++ Love-calculator.js
@@ -0,0 +1,12 @@
+prompt("What is your name ?")
+prompt("What is lover's name ?")
+
+var lovescore=Math.random()*100;
+lovescore=Math.floor(lovescore)+1;
+if (lovescore===100){
+     alert("Your score is "+lovescore+ "%. You Guys are great lovers ❤️ :)")
+}else if (lovescore<70||lovescore>20 ){
+     alert("Your score is "+lovescore+"% .You Guys love as much as you can.")
+}else {
+    alert("your score is "+lovescore+"%. You should put more effort in love.")
+}
