aad89dff7598f4d658da94a5c12021e41b2e2cd0
SanjayMohan <sanjaykakkatiri@gmail.com>
2020-08-20 00:56:20 +0530

    final

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,11 +1,15 @@
 const express = require("express")
 const app = express()
-
+const bodyParser = require("body-parser")
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.get("/",function(req,res){
-    app.use(express.static(__dirname+"/styles.css"))
     res.sendFile(__dirname+"/index.html")
-}
-)
-
+})
+app.post("/",function(req,res){
+    var p1=Number(req.body.number1)
+    var p2=Number(req.body.number2)
+    var p3=p1+p2
+    res.send("result is "+p3)
+})
 app.listen(3000)
