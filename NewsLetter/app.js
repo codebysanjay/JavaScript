f31eaeb454242dcfbc63bf0eb3b7bdeca24a610c
SanjayMohan <sanjaykakkatiri@gmail.com>
2020-08-24 21:53:51 +0530

    Newsletter signup

--- NewsLetter/app.js
+++ NewsLetter/app.js
@@ -2,6 +2,19 @@ const express= require("express");
 const bodyParser=require("body-parser");
 const request=require("request");
 
-const app= express()
+const app= express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
 
-app.listen(3000)
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/signup.html");
+});
+
+app.post("/",function(req,res){
+    var firstN = req.body.first;
+    var secondN = req.body.second;
+    var emailN = req.body.email;
+    console.log(firstN,secondN,emailN);
+});
+
+app.listen(3000);
