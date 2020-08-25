d71e524b5cc6f657ca04b26e925fa737482e27db
SanjayMohan <sanjaykakkatiri@gmail.com>
2020-08-25 20:56:27 +0530

    final

--- NewsLetter/app.js
+++ NewsLetter/app.js
@@ -21,7 +21,7 @@ app.post("/",function(req,res){
                 status:"subscribed",
                 merge_fields:{
                     FNAME:firstN,
-                    LNAME:secondN,
+                    LNAME:secondN
                 }
             }
         ]
@@ -36,17 +36,23 @@ app.post("/",function(req,res){
             "Authorization":"aagneyagecb 1ef6119150ce8537f983f1f3511076ba-us17"
             
         },
-        body:jsonData
+        // body:jsonData
     };
     request(options,function(error,response,body){
         if (error){
             console.log(error);
         }else{
-            console.log(response.statusCode);
+            if(response.statusCode===200){
+                res.sendFile(__dirname+"/success.html");
+            }else{
+                res.sendFile(__dirname+"/failure.html");
+            }
         }
     });
 });
-
+app.post("/failure",function(req,res){
+    res.redirect("/");
+})
 app.listen(3000);
 
 
