f5233e859338b1d630ddd83c027fc6935a450b8d
SanjayMohan <sanjaykakkatiri@gmail.com>
2020-08-25 19:50:08 +0530

    app js

--- NewsLetter/app.js
+++ NewsLetter/app.js
@@ -14,7 +14,42 @@ app.post("/",function(req,res){
     var firstN = req.body.first;
     var secondN = req.body.second;
     var emailN = req.body.email;
-    console.log(firstN,secondN,emailN);
+    var data = {
+        members:[
+            {
+                email_address:emailN,
+                status:"subscribed",
+                merge_fields:{
+                    FNAME:firstN,
+                    LNAME:secondN,
+                }
+            }
+        ]
+    }
+
+    var jsonData = JSON.stringify(data);
+
+    var options = {
+        url:"https://us17.api.mailchimp.com/3.0/lists/e701086c34",
+        method:"POST",
+        headers:{
+            "Authorization":"aagneyagecb 1ef6119150ce8537f983f1f3511076ba-us17"
+            
+        },
+        body:jsonData
+    };
+    request(options,function(error,response,body){
+        if (error){
+            console.log(error);
+        }else{
+            console.log(response.statusCode);
+        }
+    });
 });
 
 app.listen(3000);
+
+
+// 1ef6119150ce8537f983f1f3511076ba-us17
+
+// e701086c34
